021d8b0a0e45a0bf5ea9e154c96120d490eb3e73
nnesnidal <96435379+nnesnidal@users.noreply.github.com>
2022-01-14 20:57:14 +0100

    Add files via upload

--- infest/infest/pretrazivac.php
+++ infest/infest/pretrazivac.php
@@ -1,14 +1,15 @@
 <script>
-    
     function odabraniMain(id, odabran) {
         if ($('#'+id).length === 0) {
+            if ($(".odabrani").length < 40) {
             //var redak = "<div class='redak' id='redak"+id+"'></div>";
             var odabrani = "<div class='odabrani' id='"+id+"'></div>";
-            var obrisiOdabranog = "<a class='obrisiOdabranog' id='obrisi"+id+"'>"+ odabran +"</a>";
+            var obrisiOdabranog = "<a class='obrisiOdabranog obrisiOdabranogStyle' id='obrisi"+id+"'>"+ odabran +"</a>";
             //$('#odabrani_container').append(redak);
             $('#odabrani_container').append(odabrani);
             //$('#odabrani_container').append(obrisiOdabranog);
             $('#'+id).append(obrisiOdabranog);
+            }
         }
         $(".obrisiOdabranog").click(function(){
             $(this).parent('div').remove();
@@ -38,7 +39,8 @@
             $arr[] = $row['MAIN'];
 echo '
 <div class="tr">
-    <div class="td">'.$row['MAIN'].'<button onclick="odabraniMain(\''.$row['id'].'\', \''.$row['MAIN'].'\');" class="plus"><i class="fas fa-plus"></i></button></div>
+    <div class="td">'.$row['MAIN'].'</div>
+    <div class="td"><button onclick="odabraniMain(\''.$row['id'].'\', \''.$row['MAIN'].'\');" class="plus"><i class="fas fa-plus"></i></button></div>
 </div>
 ';
         }
